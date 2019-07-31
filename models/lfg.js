17bbee620ebcbf7e2e4804de60d3bba68b4d4e4e
samy takkish <takkish_23@hotmail.com>
2019-07-31 12:00:36 -0700

    changed datta base to sequelize added model folder and index.js/lfg.js
    added require moment.js

---
+++ models/lfg.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+    var gamePlay = sequelize.define("gamePlay", {
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }, platform: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }, game_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }, userText: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        mic: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },created_at: {
+            type: DataTypes.date,
+            allowNull: false
+        },
+    });
+    return gamePlay;
+};
+
+
+
+
+
