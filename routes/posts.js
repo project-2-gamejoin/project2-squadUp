f1efb9bf7da0933b90fd357e6637e8adab5c4c30
samy takkish <takkish_23@hotmail.com>
2019-08-02 10:33:28 -0700

    added moment.js and function to remove posts after 1 day tested in postman

--- routes/posts.js
+++ routes/posts.js
@@ -1,20 +1,33 @@
 var db = require("../models");
+var moment = require('moment');
+moment().format();
+const { Op } = require('sequelize');
 
-module.exports = function(app) {
-  app.get("/api/posts", function(req, res) {
+// model.findAll({
+//   where: {
+//     start_datetime: {
+//       [Op.gte]: moment().subtract(7, 'days').toDate()
+//     }
+//   }
+// })
+
+module.exports = function (app) {
+  app.get("/api/posts", function (req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
     db.gamePlay.findAll({
       where: {
-        createdAt: req.params.id
+        createdAt: {
+          [Op.gte]: moment().subtract(1, 'days').toDate()
+        }
       }
-    }).then(function(dbgamePlay) {
+    }).then(function (dbgamePlay) {
       res.json(dbgamePlay);
     });
   });
 
-  app.get("/api/authors/:game_name", function(req, res) {
+  app.get("/api/authors/:game_name", function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
@@ -23,24 +36,14 @@ module.exports = function(app) {
         game_name: req.params.game_name
       },
       include: [db.Post]
-    }).then(function(dbgamePlay) {
+    }).then(function (dbgamePlay) {
       res.json(dbgamePlay);
     });
   });
 
-  app.post("/api/make", function(req, res) {
-    db.gamePlay.create(req.body).then(function(dbgamePlay) {
+  app.post("/api/make", function (req, res) {
+    db.gamePlay.create(req.body).then(function (dbgamePlay) {
       res.json(dbgamePlay);
-    });
-  });
-
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
     });
   });
 
