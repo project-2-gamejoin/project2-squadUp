f076607f412d3e96b5d41136ff9c28e744f46772
Gun Lee <ghun22322@gmail.com>
2019-08-02 10:05:57 -0700

    add some express

---
+++ config/connection.js
@@ -0,0 +1,41 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+//read data
+var mysq2 = require("mysq2");
+
+var connection = mysq2.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "games_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+
+});
+
+function Games(id, name) {
+    this.id = id;
+    this.name = name;
+}
+
+function Users(id, name) {
+    this.id = id;
+    this.name = name;
+
+}
+
+
+function afterConnection() {
+    connection.query("SELECT * FROM games_db.gamePlay", function (err, res) {
+        if (err) throw err;
+
+        console.log(res);
+        connection.end();
+    });
+}
